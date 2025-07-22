65b573a69ba0cb7731798a6741acbb37b2b5d9fd
Olivier FATOMBI <oliverfatombi@gmail.com>
2025-07-22 12:11:42 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:first_app/controllers/theme_controller.dart';
+import 'package:first_app/utils/app_themes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  await GetStorage.init();
+  Get.put(ThemeController());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeController = Get.find<ThemeController>();
+
+    return GetMaterialApp(
+      title: 'Fashion Store',
+      theme: AppThemes.light,
+      darkTheme: AppThemes.dark,
+      themeMode: themeController.theme,
+      defaultTransition: Transition.fade,
+    );
+  }
+}
